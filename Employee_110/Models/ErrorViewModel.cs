26161304a66dca0ef987553c3e17620059df6e86
wachidmuslih <wachidmslh@gmail.com>
2021-10-18 21:32:45 +0700

    Add project files.

---
+++ Employee_110/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Employee_110.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
